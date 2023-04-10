86907946a223936ca53400c1b6b34fc37a9de1ac
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-04-10 09:00:22 +0900

    Initial Commit

---
+++ ChatGPTL/ChatGPTLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTLApp.swift
+//  ChatGPTL
+//
+//  Created by Yasutaka Kawamoto on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
